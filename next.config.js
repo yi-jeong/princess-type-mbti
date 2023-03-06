2ca9e383c3257d395ffe3f354d548214d1444c0f
yi-jeong <o414_h@naver.com>
2023-03-06 14:27:53 +0900

    파일 삭제

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? '/princess-type-mbti/' : '',
+  basePath: '/princess-type-mbti'
 }
 
 module.exports = nextConfig
